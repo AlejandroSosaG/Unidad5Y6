fc33f8b7156d6d9274e88f2ebdb974424a6bf1fe
AlejandroSosaG <alejandro.sosa@iesnervion.es>
2023-10-05 14:33:20 +0200

    Agregar archivos de proyecto.

---
+++ HolaMundo_Maui/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Biblioteca;
+
+namespace HolaMundo_Maui
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        private clsPersona persona = new clsPersona();
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+            string apellidos= await DisplayPromptAsync("Hello ", "Apellidos: ");
+            persona.Nombre = txtNombre.Text;
+            persona.Apellidos = apellidos;
+            if (!string.IsNullOrEmpty(persona.Nombre) && !string.IsNullOrEmpty(persona.Apellidos))
+            {
+                Hola.Text = "Hello " + persona.NombreCompleto;
+                await DisplayAlert("Hola", persona.NombreCompleto, "ok");
+            }
+            else Hola.Text = "Hello World";
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+}
